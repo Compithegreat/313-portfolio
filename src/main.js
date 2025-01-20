6adc6f743377dc666636599a0d9d9788a6b2a9a6
Compithegreat <gianluca.crossbussoli@gmail.com>
2025-01-20 00:05:49 -0700

    updated favicon

--- src/main.js
+++ src/main.js
@@ -170,7 +170,8 @@ When I wake up, when I disembark this train, I will be met with a world unfamili
         </div>
       </div> 
       
-      
+      <!--
+
       <div id="project-row">
         <div id="titler">
           <h3>Assignment 2</h3>
@@ -192,6 +193,9 @@ When I wake up, when I disembark this train, I will be met with a world unfamili
           <h4 id="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </h4>
         </div>
       </div>
+      
+      -->
+
       <!--
       <div id="project-row">
         <div id="titler">
